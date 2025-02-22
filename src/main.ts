0685f1a9fce57495d30ba22ce2fa38f77ba91d5e
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-22 23:36:38 +0300

    Init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import "./style.css";
+import App from "./App.vue";
+
+createApp(App)
+	.use(PrimeVue, { theme: { preset: Aura } })
+	.mount("#app");
