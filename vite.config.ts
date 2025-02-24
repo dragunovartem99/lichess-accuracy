0e46ac01cbf4ab6b12664952128523df04fd6893
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-25 00:17:30 +0300

    Add deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "./",
 	plugins: [
 		vue({
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag === "html-diagram"
-				},
-			},
+			template: { compilerOptions: { isCustomElement: (tag) => tag === "html-diagram" } },
 		}),
 	],
 });
