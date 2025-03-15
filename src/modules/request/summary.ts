5422219d2b87ceb727d6bb99b48b4efe30ef37a2
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-15 09:08:02 +0300

    Improve request summary

--- src/modules/request/summary.ts
+++ src/modules/request/summary.ts
@@ -3,23 +3,29 @@ import { options } from "./options";
 import { gameVariants } from "@/static/gameVariants";
 
 export const summary = computed(() => {
+	const gameLimit = options.value.max || "All";
+
 	const rated = new Map([
 		[true, "rated"],
 		[false, "casual"],
 		[null, null],
 	]).get(options.value.rated);
 
+	const color = options.value.color && `as ${options.value.color}`;
+
+	const chosenVariants = options.value.perfType;
+
+	const variantsLabels = chosenVariants.map((chosen) => {
+		const variant = gameVariants.find((variant) => variant.value === chosen)!;
+		return variant.label.toLowerCase();
+	});
+
 	const variants =
-		!options.value.perfType.length || options.value.perfType.length === gameVariants.length
+		!chosenVariants.length || chosenVariants.length === gameVariants.length
 			? "All variants"
-			: "Variants: " + options.value.perfType.join(", ");
+			: "Variants: " + variantsLabels.join(", ");
 
-	const chunks = [
-		options.value.max || "All",
-		rated,
-		"games",
-		options.value.color && `as ${options.value.color}`,
-	];
+	const chunks = [gameLimit, rated, "games", color].filter((chunk) => !!chunk).join(" ");
 
-	return chunks.filter((chunk) => chunk !== null).join(" ") + ". " + variants + ".";
+	return chunks + ". " + variants + ".";
 });
